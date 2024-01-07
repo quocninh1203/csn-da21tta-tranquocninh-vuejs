c05736dc737565eedf46177f6cd60b97f1f4301f
quocninh <quocninh9.32017@gmail.com>
2024-01-08 02:05:09 +0700

    update

--- scr/src/main.js
+++ scr/src/main.js
@@ -1,16 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import './assets/themify-icons/themify-icons.css'
 import router from './router'
-import store from './store/store';
+import store from './store/store.js';
 
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-
-
 const vuetify = createVuetify({
     components,
     directives
